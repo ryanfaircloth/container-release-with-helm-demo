8a453ad3bb390f88e9afd08e7c577e982a7a7d97
Ryan Faircloth <ryan@dss-i.com>
2022-07-01 16:33:29 -0400

    feat: with cart

--- .github/workflows/scripts/prepareHelm.sh
+++ .github/workflows/scripts/prepareHelm.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 #
-yq -i ".version = \"${1}\" | .appVersion style=\"double\" | .appVersion = \"${1}\"" charts/s3proxy/Chart.yaml
-helm package charts/s3proxy
+yq -i ".version = \"${1}\" | .appVersion style=\"double\" | .appVersion = \"${1}\"" charts/$GITHUB_REPOSITORY/Chart.yaml
+helm package charts/$GITHUB_REPOSITORY
