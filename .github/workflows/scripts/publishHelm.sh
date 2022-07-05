4d2655b84b52ba8a4afdd4b267e272863a4fdff7
Ryan Faircloth <ryan@dss-i.com>
2022-07-05 13:00:15 -0400

    fix: upload to release

--- .github/workflows/scripts/publishHelm.sh
+++ .github/workflows/scripts/publishHelm.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 set -ev
-#helm push $(ls ${GITHUB_REPOSITORY#*/}*.tgz) oci://ghcr.io/$GITHUB_REPOSITORY/charts
+helm push $(ls ${GITHUB_REPOSITORY#*/}*.tgz) oci://ghcr.io/$GITHUB_REPOSITORY/charts
+gh release upload $1 $(ls ${GITHUB_REPOSITORY#*/}*.tgz)
