ee7d6ea4942d1b6483117d26d3ea178e889dc323
Ryan Faircloth <ryan@dss-i.com>
2022-07-05 14:15:40 -0400

    fix: change variable for owner

--- .github/workflows/scripts/publishHelm.sh
+++ .github/workflows/scripts/publishHelm.sh
@@ -3,11 +3,11 @@ set -ev
 helm push $(ls ${GITHUB_REPOSITORY#*/}*.tgz) oci://ghcr.io/$GITHUB_REPOSITORY/charts
 
 mkdir /tmp/gh-pages
-wget https://${GITHUB_OWNER}.github.io/${GITHUB_REPOSITORY#*/}/index.yaml -P /tmp/
+wget https://${GITHUB_REPOSITORY_OWNER}.github.io/${GITHUB_REPOSITORY#*/}/index.yaml -P /tmp/
 helm repo index . --url https://github.com/${GITHUB_REPOSITORY}/releases/download/$1 --merge /tmp/index.yaml
 cp -f index.yaml /tmp/index.yaml
 
-gh repo clone ${GITHUB_REPOSITORY#*/} /tmp/gh-pages/
+gh repo clone ${GITHUB_REPOSITORY} /tmp/gh-pages/
 pushd /tmp/gh-pages
 cp -f /tmp/index.yaml .
 git add index.yaml
