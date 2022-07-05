e617aa80a2af11135c446bf9379323927aca78d7
Ryan Faircloth <ryan@dss-i.com>
2022-07-05 15:36:40 -0400

    fix: auth

--- .github/workflows/scripts/publishHelm.sh
+++ .github/workflows/scripts/publishHelm.sh
@@ -9,6 +9,7 @@ cp -f index.yaml /tmp/index.yaml
 #
 gh repo clone ${GITHUB_REPOSITORY} /tmp/gh-pages/
 pushd /tmp/gh-pages
+gh auth setup-git
 cp -f /tmp/index.yaml .
 git add index.yaml
 git commit -m "chore(helm): Publish $1"
