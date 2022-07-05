b3ed9ec77ce4e3ec95bffd89355a745d5264da6b
Ryan Faircloth <ryan@dss-i.com>
2022-07-05 14:27:49 -0400

    fix: try again

--- .github/workflows/scripts/publishHelm.sh
+++ .github/workflows/scripts/publishHelm.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ev
 helm push $(ls ${GITHUB_REPOSITORY#*/}*.tgz) oci://ghcr.io/$GITHUB_REPOSITORY/charts
-
+#
 mkdir /tmp/gh-pages
 wget https://${GITHUB_REPOSITORY_OWNER}.github.io/${GITHUB_REPOSITORY#*/}/index.yaml -P /tmp/
 helm repo index . --url https://github.com/${GITHUB_REPOSITORY}/releases/download/$1 --merge /tmp/index.yaml
